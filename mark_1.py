d2f3365c1179387dd6ccd0abf6907ae895a6357c
Connor Mackay <connor.mackay@gmail.com>
2023-08-01 08:11:31 -0400

    add logic to build townhalls and workers

--- mark_1.py
+++ mark_1.py
@@ -3,11 +3,23 @@ from sc2.player import Bot, Computer
 from sc2.main import run_game
 from sc2.data import Race, Difficulty
 from sc2.bot_ai import BotAI
+from sc2.ids.unit_typeid import UnitTypeId
+
+
 
 class WorkerRushBot(BotAI):
     async def on_step(self, iteration: int):
         print("currently on interation:", iteration)
-        
+
+        if self.townhalls:
+            nexus = self.townhalls.random()
+        else:
+            if self.can_afford(UnitTypeId.NEXUS):
+                await self.expand_now()
+                
+        if nexus.is_idle and self.can_afford(UnitTypeId.PROBE):
+            nexus.train(UnitTypeId.PROBE)
+
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, WorkerRushBot()),
