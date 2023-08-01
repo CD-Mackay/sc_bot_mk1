b070a480548bead076cff154273322984c9cf799
Connor Mackay <connor.mackay@gmail.com>
2023-08-01 08:20:35 -0400

    implement logic to expand base

--- mark_1.py
+++ mark_1.py
@@ -4,6 +4,7 @@ from sc2.main import run_game
 from sc2.data import Race, Difficulty
 from sc2.bot_ai import BotAI
 from sc2.ids.unit_typeid import UnitTypeId
+import random
 
 
 
@@ -19,6 +20,12 @@ class WorkerRushBot(BotAI):
         elif not self.structures(UnitTypeId.PYLON) and self.already_pending(UnitTypeId.PYLON) == 0:
             if self.can_afford(UnitTypeId.PYLON):
               await self.build(UnitTypeId.PYLON, near=nexus)
+
+        elif self.strucutres(UnitTypeId.PYLON).amount < 5:
+            if self.can_afford(UnitTypeId.PYLON):
+                target_pylon = self.structures(UnitTypeId.PYLON).closest_to(self.enemy_start_locations[0])
+                pos = target_pylon.position.towards(self.enemy_start_locations[0], random.randrange([8, 15]))
+                await self.build(UnitTypeId.PYLON, near=pos)
             
         else:
           if self.can_afford(UnitTypeId.NEXUS):
